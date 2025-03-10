5e26e79951b38fa5da6939da568c9bfd66371d7c
SteveAustin79 <steveaustin79@proton.me>
2025-03-10 21:23:17 +0100

    init version

--- YTDLa.py
+++ YTDLa.py
@@ -267,10 +267,10 @@ def user_selection(u_lines, show_latest_video_date):
 
     latest_date_formated = ""
 
-    temp_disable = smart_input("Disable latest video date for this run?  Y/n", "n")
-    print()
-    if temp_disable == "y":
-        show_latest_video_date = False
+    # temp_disable = smart_input("Disable latest video date for this run?  Y/n", "n")
+    # print()
+    # if temp_disable == "y":
+    show_latest_video_date = False
 
     print("Select channel:")
     for index, line in enumerate(u_lines, start=1):
