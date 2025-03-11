4e70c48bb7fc98fb1a21e7bb1f1e9e4111ec6177
SteveAustin79 <steveaustin79@proton.me>
2025-03-11 21:44:11 +0100

    init version

--- YTDLa.py
+++ YTDLa.py
@@ -835,10 +835,9 @@ while True:
                             video_watch_urls.append(url.watch_url)
                     else:
                         video_watch_urls.append(url.watch_url)
-                print(f"\rFetching videos: " + url.video_id, end="", flush=True)
-
-            print(f'\n\nTotal {count_total_videos} Video(s) by: \033[96m{c.channel_name}\033[0m\n')
-
+                print(f"\rFetching " + str(count_total_videos) + " videos", end="", flush=True)
+            print(f"\rTotal {count_total_videos} Video(s) by: \033[96m{c.channel_name}\033[0m", end="", flush=True)
+            print()
         for url in video_watch_urls:
             only_video_id = pytubefix.extract.video_id(url)
 
