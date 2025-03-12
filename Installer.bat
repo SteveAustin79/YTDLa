ade9dd91ff996bfc1575616364ce7950d6bf8402
SteveAustin79 <steveaustin79@proton.me>
2025-03-12 15:37:34 +0100

    init version

--- Installer.bat
+++ Installer.bat
@@ -1,7 +1,7 @@
 @echo off
 start Sources\Git-2.48.1-64-bit.exe
 echo Install git by clicking through setup (always select "Next")
-echo Once Installation complete, press Enter
+echo When git installation completed, press Enter
 pause
 start Sources\python-3.13.2-amd64.exe
 pause
