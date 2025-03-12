c52394c008cda5dbf272653fa89fbe0e566e7fb4
SteveAustin79 <steveaustin79@proton.me>
2025-03-12 21:44:21 +0100

    init version

--- Installer.bat
+++ Installer.bat
@@ -3,13 +3,13 @@ REM echo %HOMEDRIVE%
 REM echo %HOMEPATH%
 REM echo %CD%
 
-SET GIT="%CD%\Sources\PortableGit\bin"
-SET FFMPEG="%CD%\Sources\ffmpeg-master-latest-win64-gpl\bin"
-SET PYTHON="%CD%"\venv\Scripts
+REM SET GIT="%CD%\Sources\PortableGit\bin"
+REM SET FFMPEG="%CD%\Sources\ffmpeg-master-latest-win64-gpl\bin"
+REM SET PYTHON="%CD%"\venv\Scripts
 
 REM setx PATH "%GIT%;%FFMPEG%;%PYTHON%" /m
-setx PATH "%GIT%;%FFMPEG%;%PYTHON%"
+REM setx PATH "%GIT%;%FFMPEG%;%PYTHON%"
 
-ffmpeg
+REM ffmpeg
 
 pause
