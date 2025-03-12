5b891abd2731502cda20dd758d8df13ca6ce449f
SteveAustin79 <steveaustin79@proton.me>
2025-03-12 01:01:46 +0100

    init version

--- YTDLa.py
+++ YTDLa.py
@@ -8,7 +8,7 @@ import pytubefix.extract
 from pytubefix import YouTube, Channel
 from pytubefix.cli import on_progress
 
-version = "0.2 (20250311)"
+version = "0.2 (20250312)"
 header_width_global = 85
 
 class BCOLORS:
