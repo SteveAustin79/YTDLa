19051f66cd9968b4cd6fa77f90820c14eee8d95c
SteveAustin79 <steveaustin79@proton.me>
2025-03-13 14:13:25 +0100

    Updated YTDLa

--- Installer.bat
+++ Installer.bat
@@ -7,8 +7,7 @@ REM SET GIT="%CD%\Sources\PortableGit\bin"
 REM SET FFMPEG="%CD%\Sources\ffmpeg-master-latest-win64-gpl\bin"
 REM SET PYTHON="%CD%"\venv\Scripts
 
-REM setx PATH "%GIT%;%FFMPEG%;%PYTHON%" /m
-REM setx PATH "%GIT%;%FFMPEG%;%PYTHON%"
+REM setx PATH "%PATH%;%GIT%;%FFMPEG%;%PYTHON%"
 
 REM ffmpeg
 
