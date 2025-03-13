eb60774dfb231604403f74765ea7371a702d9e9b
SteveAustin79 <steveaustin79@proton.me>
2025-03-13 23:34:51 +0100

    Updated YTDLa

--- Start.bat
+++ Start.bat
@@ -1,2 +1,3 @@
 @echo off
+venv\Scripts\python.exe -m pip install pytubefix ffmpeg-python
 venv\Scripts\python.exe YTDLa.py
--- _Setup2.ps1
+++ _Setup2.ps1
@@ -1,7 +1,7 @@
 Write-Host ""
 Write-Host "Welcome to the YTDLa Windows 10/11 Installation Script! - PART 2/2" -ForegroundColor Cyan
 Write-Host ""
-Write-Host "This script will create virtual Python environment (venv) and install dependencies."
+Write-Host "This script will create virtual Python environment (venv)."
 Write-Host ""
 Write-Host "Estimated duration: 20 Seconds"
 Write-Host ""
@@ -9,10 +9,8 @@ Read-Host "Press ENTER to continue..."
 
 Start-Process -NoNewWindow -Wait -FilePath "python" -ArgumentList "-m venv venv"
 
-Start-Process -NoNewWindow -Wait -FilePath "python" -ArgumentList "-m pip install pytubefix ffmpeg-python"
-
 Write-Host ""
-Write-Host "Virtual Python environment created and dependencies installed!"
+Write-Host "Virtual Python environment created!"
 Write-Host ""
 Write-Host "Open YTDLa with Start.bat. To update the app start Update.bat."
 Read-Host "Press ENTER to continue..."
